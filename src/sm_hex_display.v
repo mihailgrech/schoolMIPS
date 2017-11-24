02d6f97cbc0420252c4b0195ee1fbe2a007d10f5
mihailgrech <mixailgrech@yandex.ru>
2017-11-24 22:33:08 +0300

    modules for the BC56-12GWA 7 digit indicator

--- src/sm_hex_display.v
+++ src/sm_hex_display.v
@@ -4,29 +4,52 @@ module sm_hex_display
     input      [3:0] digit,
     output reg [6:0] seven_segments
 );
-
+    // BC56-12GWA 7-digit indicator
     always @*
         case (digit)
-        'h0: seven_segments = 'b1000000;  // a b c d e f g
-        'h1: seven_segments = 'b1111001;
-        'h2: seven_segments = 'b0100100;  //   --a--
-        'h3: seven_segments = 'b0110000;  //  |     |
-        'h4: seven_segments = 'b0011001;  //  f     b
-        'h5: seven_segments = 'b0010010;  //  |     |
-        'h6: seven_segments = 'b0000010;  //   --g--
-        'h7: seven_segments = 'b1111000;  //  |     |
-        'h8: seven_segments = 'b0000000;  //  e     c
-        'h9: seven_segments = 'b0011000;  //  |     |
-        'ha: seven_segments = 'b0001000;  //   --d-- 
-        'hb: seven_segments = 'b0000011;
-        'hc: seven_segments = 'b1000110;
-        'hd: seven_segments = 'b0100001;
-        'he: seven_segments = 'b0000110;
-        'hf: seven_segments = 'b0001110;
+        'h0: seven_segments = 'b0111111;  // g f e d c b a
+        'h1: seven_segments = 'b0000110;  // 
+        'h2: seven_segments = 'b1011011;  //   --a--
+        'h3: seven_segments = 'b1001111;  //  |     |
+        'h4: seven_segments = 'b1100110;  //  f     b
+        'h5: seven_segments = 'b1101101;  //  |     |
+        'h6: seven_segments = 'b1111101;  //   --g--
+        'h7: seven_segments = 'b0000111;  //  |     |
+        'h8: seven_segments = 'b1111111;  //  e     c
+        'h9: seven_segments = 'b1100111;  //  |     |
+        'ha: seven_segments = 'b1110111;  //   --d-- 
+        'hb: seven_segments = 'b1111100;
+        'hc: seven_segments = 'b0111001;
+        'hd: seven_segments = 'b1011110;
+        'he: seven_segments = 'b1111001;
+        'hf: seven_segments = 'b1110001;
         endcase
-
 endmodule
 
+module sm_hex_display_blink
+(
+    input [6:0] digit1,
+    input [6:0] digit2,
+    input [6:0] digit3,
+    input       clkIn, // wire to clk100 or smth else
+    output reg [11:0] seven_segments
+);
+
+    reg [9:0] count = 10'b0000000000;
+    
+    always @(posedge clkIn)
+    begin
+        case (count [9:8])
+            2'b00: seven_segments = { 1'b0, digit1[0], digit1[5], 2'b11, digit1[1], 1'b1, digit1[6], digit1[2], 1'b0, digit1[3], digit1[4] };
+            2'b01: seven_segments = { 1'b1, digit2[0], digit2[5], 2'b01, digit2[1], 1'b1, digit2[6], digit2[2], 1'b0, digit2[3], digit2[4] };
+            2'b10: seven_segments = { 1'b1, digit3[0], digit3[5], 2'b10, digit3[1], 1'b1, digit3[6], digit3[2], 1'b0, digit3[3], digit3[4] };
+            default: count <= count + 1'b0;
+        endcase
+        
+        count <= count + 1'b1;
+    end
+endmodule
+    
 //--------------------------------------------------------------------
 
 module sm_hex_display_8
